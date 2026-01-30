ac19060e9e570614ca14e2c448bec5efd97f6f33
Matyas Barocsai <matyas.istvan.barocsai@foi.se>
2026-01-30 09:56:27 +0100

    Monitor configuration at work

--- misc/monitor_config.sh
+++ misc/monitor_config.sh
@@ -1,3 +1,3 @@
-xrandr --output DP-2 --auto --pos 0x0 --primary
-xrandr --output DP-1 --auto --pos 2560x0 
-xrandr --output HDMI-1 --auto --pos 5120x0 
+xrandr --output DP-1 --off --output DP-1 --mode 2560x1440 --pos 0x0 --primary
+xrandr --output DP-2 --off --output DP-2 --mode 2560x1440 --pos 2560x0 
+xrandr --output HDMI-1 --off --output HDMI-1 --mode 1280x1024 --pos 5120x0 
